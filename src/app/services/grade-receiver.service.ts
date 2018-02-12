64a2c6fd7bbf38b9437c5306be17aa380fc9a31d
justin00892 <justin00892@gmail.com>
2018-02-12 10:03:00 -0500

    Synced Changes

--- src/app/services/grade-receiver.service.ts
+++ src/app/services/grade-receiver.service.ts
@@ -7,36 +7,14 @@ import { Observable } from 'rxjs/Observable';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 import { sampleJson } from '../../../sample semester';
+import { Session } from 'selenium-webdriver';
 
 @Injectable()
 export class GradeReceiverService implements OnInit{
-  semesters: Observable<any[]>;
+  semesters: Observable<any>;
   constructor(private db: AngularFireDatabase) { }
   ngOnInit() {
     this.semesters = this.getSemesters('/semesters');
-    console.log(this.semesters)
-  }
-  createSemesterObject(semester: Semester) {
-    let courses = [];
-    semester.courses.forEach((course) => {
-      let groups = [];
-      course.gradeGroups.forEach((group) => {
-        let grades = [];
-        group.grades.forEach((grade) => {
-          grade = new Grade(grade);
-          grades.push(grade);
-        }); // end grades loop;
-        group.grades = grades;
-        group = new GradeGroup(group);
-        groups.push(group);
-      }); // end GradeGroup loop
-      course.gradeGroups = groups;
-      course = new Course(course);
-      courses.push(course);
-    }); // end Courses loop
-    semester.courses = courses;
-    let newSemester = new Semester(semester);
-    return newSemester;
   }
 
   getSemesterById(id) {
@@ -44,7 +22,7 @@ export class GradeReceiverService implements OnInit{
   }
 
   getSemesters(path): Observable<any[]> {
-    return this.db.list<Semester>(path).valueChanges();
+    return this.db.list(path).valueChanges();
   }
 
 }
